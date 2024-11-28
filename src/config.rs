58fd0c5e0b4093644c0169495f2ebab28760277a
Rie Takahashi <megumin@megu.dev>
2024-11-28 20:28:05 +0000

    feat: v2/linux support

---
+++ src/config.rs
@@ -0,0 +1,221 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ConfigFile {
+    Instance(Instance),
+    Mod(Mod),
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub mods: HashMap<String, Mod>,
+    pub instances: HashMap<String, Instance>,
+}
+
+impl Config {
+    pub fn validate(&mut self) {
+        for (_, mod_) in self.mods.iter_mut() {
+            let entrypoint_exists = std::path::Path::new(&mod_.path)
+                .join(&mod_.entrypoint)
+                .exists();
+
+            mod_.is_valid = entrypoint_exists;
+        }
+
+        for (_, instance) in self.instances.iter_mut() {
+            if let Some(target_mod) = self.mods.get(&instance.r#mod) {
+                let asar_exists: bool = std::path::Path::new(&instance.path)
+                    .parent()
+                    .expect("Expected Discord executable path to have a parent.")
+                    .join("resources")
+                    .join("app.asar")
+                    .exists();
+
+                instance.is_valid = target_mod.is_valid && asar_exists;
+            } else {
+                instance.is_valid = false;
+            }
+        }
+    }
+
+    pub fn init() -> Config {
+        let profiles_dir = dirs::data_local_dir().unwrap().join("discord-modloader");
+
+        println!("Loading from profile directory: {}", profiles_dir.display());
+
+        if !profiles_dir.exists() {
+            std::fs::create_dir_all(&profiles_dir).expect("Unable to create profiles directory.");
+        }
+
+        let configs_dir = dirs::config_local_dir().unwrap().join("discord-modloader");
+
+        let instances_dir = configs_dir.join("instances");
+        let mods_dir = configs_dir.join("mods");
+
+        if !instances_dir.exists() {
+            std::fs::create_dir_all(&instances_dir).expect("Unable to create instances directory.");
+        }
+
+        if !mods_dir.exists() {
+            std::fs::create_dir_all(&mods_dir).expect("Unable to create mods directory.");
+        }
+
+        let instances = std::fs::read_dir(&instances_dir).unwrap();
+        let mods = std::fs::read_dir(&mods_dir).unwrap();
+
+        println!("Loading instances from: {:?}", &instances_dir);
+
+        let mut mod_configs: HashMap<String, Mod> = HashMap::new();
+        for mod_file in mods {
+            let mod_path = mod_file.unwrap();
+            let mod_file = ConfigFile::from_file(mod_path.path().to_str().unwrap());
+            let mod_name = mod_path
+                .path()
+                .with_extension("")
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string();
+
+            if let ConfigFile::Mod(mut mod_file) = mod_file {
+                mod_file.config_path = mod_path
+                    .path()
+                    .canonicalize()
+                    .expect("Unable to locate config file.")
+                    .to_str()
+                    .unwrap()
+                    .to_string();
+                mod_configs.insert(mod_name, mod_file);
+            } else {
+                panic!("Instance file found in mods directory.");
+            }
+        }
+
+        let mut instance_configs: HashMap<String, Instance> = HashMap::new();
+        for instance_file in instances {
+            let instance_path = instance_file.unwrap();
+            let instance_file = ConfigFile::from_file(instance_path.path().to_str().unwrap());
+            let instance_name = instance_path
+                .path()
+                .with_extension("")
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string();
+
+            if let ConfigFile::Instance(mut instance_file) = instance_file {
+                instance_file.config_path = instance_path
+                    .path()
+                    .canonicalize()
+                    .expect("Unable to locate config file.")
+                    .to_str()
+                    .unwrap()
+                    .to_string();
+
+                instance_file.profile_path = instance_file
+                    .profile
+                    .as_ref()
+                    .map(|profile| profiles_dir.join(profile).to_str().unwrap().to_string());
+
+                instance_configs.insert(instance_name, instance_file);
+            } else {
+                panic!("Mod file found in instances directory.");
+            }
+        }
+
+        Config {
+            mods: mod_configs,
+            instances: instance_configs,
+        }
+    }
+}
+
+impl ConfigFile {
+    fn from_file(path: &str) -> Self {
+        let file = std::fs::read_to_string(path).unwrap();
+        let config: ConfigFile = toml::from_str(&file).unwrap();
+        config
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Instance {
+    /// The display name of the instance. (e.g. "Vencord", "Moonlight", "My Custom Instance")
+    ///
+    /// Can be duplicate, but not recommended for clarity.
+    pub name: String,
+
+    /// A path to the icon to use for the mod.
+    pub icon: Option<String>,
+
+    /// The custom profile to use.
+    /// This will run a unique chrome profile for the mod,
+    /// allowing for multiple instances of Discord to run at the same time.
+    pub profile: Option<String>,
+
+    /// The identifier of the mod to use for this instance.
+    pub r#mod: String,
+
+    // pub description: String,
+    pub path: String,
+
+    #[serde(skip, default)]
+    pub is_valid: bool,
+
+    #[serde(skip, default)]
+    pub config_path: String,
+
+    #[serde(skip, default)]
+    pub profile_path: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Mod {
+    /// The display name of the mod. (e.g. "Vencord", "Moonlight", "BetterDiscord")
+    ///
+    /// Can be duplicate, but not recommended for clarity.
+    pub name: String,
+
+    /// A path to the icon to use for the mod.
+    pub icon: Option<String>,
+
+    /// The path to the mod's dist folder. (e.g. "/path/to/moonlight")
+    pub path: String,
+
+    /// The entrypoint of the mod. (e.g. "injector.js", "patcher.js")
+    pub entrypoint: String,
+
+    /// Provide custom loader JS to be injected into the ASAR index.js.
+    pub loader: Option<ModLoader>,
+
+    #[serde(skip, default)]
+    pub is_valid: bool,
+
+    #[serde(skip, default)]
+    pub config_path: String,
+}
+
+/// The loader configuration for the mod.
+/// You can use this to specify custom JS to be in your ASAR's index.js.
+///
+/// The following variables can be used:
+///
+/// - `__CUSTOM_PROFILE_DIR__`: The directory of the custom profile.
+/// - `__MOD_ENTRYPOINT_FILE__`: The entrypoint file of the mod.
+#[derive(Debug, Deserialize)]
+pub struct ModLoader {
+    pub prefix: Option<String>,
+    pub profile: Option<String>,
+    #[serde(default = "default_require")]
+    pub require: Option<String>,
+    pub suffix: Option<String>,
+}
+
+fn default_require() -> Option<String> {
+    Some(r#"require("__MOD_ENTRYPOINT_FILE__")"#.to_string())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,100 @@
-use std::{ffi::c_void, ptr::null_mut};
+pub mod config;
+mod unix;
 
-use detours_sys::{
-    DetourAttach, DetourGetEntryPoint, DetourIsHelperProcess, DetourRestoreAfterWith,
-    DetourTransactionBegin, DetourTransactionCommit, DetourUpdateThread,
-};
+#[cfg(target_os = "linux")]
+#[allow(unused_imports)]
+pub use unix::*;
 
-use winapi::{
-    shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID},
-    um::{processthreadsapi::GetCurrentThread, winnt::DLL_PROCESS_ATTACH},
-};
-
-#[cfg(debug_assertions)]
-use winapi::um::consoleapi::AllocConsole;
-// use crate::detours::fs::create_asar_in_memory;
-
-mod detours;
-mod env;
-
-static mut O_ENTRYPOINT: *mut c_void = 0 as _;
-
-#[no_mangle]
-extern "cdecl" fn ExportedFunction() {}
-
-#[no_mangle]
-unsafe extern "stdcall" fn DllMain(
-    _hinst_dll: HINSTANCE,
-    fwd_reason: DWORD,
-    _lpv_reserved: LPVOID,
-) -> BOOL {
-    if DetourIsHelperProcess() == 1 {
-        return 1;
+pub fn get_or_write_cache(instance: &config::Instance, mod_: &config::Mod) -> String {
+    let cache_dir = dirs::cache_dir().unwrap().join("discord-modloader");
+    if !cache_dir.exists() {
+        std::fs::create_dir_all(&cache_dir).unwrap();
     }
 
-    if fwd_reason != DLL_PROCESS_ATTACH {
-        return 1;
+    let asar_dir = cache_dir.join("asar");
+    if !asar_dir.exists() {
+        std::fs::create_dir_all(&asar_dir).unwrap();
     }
 
-    DetourRestoreAfterWith();
+    let cached_asar_path = asar_dir.join(format!("{}-{}.asar", instance.r#mod, mod_.name));
+    // TODO: Figure out a way to cache, while also regenerating if the mod or instance config changes.
+    // if cached_asar_path.exists() {
+    //     return cached_asar_path.to_str().unwrap().to_owned();
+    // }
 
-    DetourTransactionBegin();
-    DetourUpdateThread(GetCurrentThread() as _);
+    static ASAR_CUSTOM_PROFILE_JS: &str = include_str!("./asar/custom_profile.js");
 
-    O_ENTRYPOINT = DetourGetEntryPoint(null_mut());
-    DetourAttach(&mut O_ENTRYPOINT, main as _);
-
-    detours::win32::init_detours();
-
-    DetourTransactionCommit();
-
-    1
-}
-
-unsafe fn main() {
-    #[cfg(debug_assertions)]
-    {
-        AllocConsole();
-        println!("[ModHook] Process Hooked");
+    if let Some(ref profile) = instance.profile_path {
+        let profile_path = std::path::Path::new(profile);
+        if !profile_path.exists() {
+            std::fs::create_dir_all(&profile_path).unwrap();
+        }
     }
-    let start_discord: extern "C" fn() = std::mem::transmute(O_ENTRYPOINT);
-    start_discord();
+
+    let mod_entrypoint = std::path::Path::new(&mod_.path).join(&mod_.entrypoint);
+    let mod_entrypoint = mod_entrypoint.to_str().unwrap();
+
+    let profile_dir = instance.profile_path.clone().unwrap_or_default();
+
+    let profile_loader = instance.profile.as_ref().map(|_| {
+        ASAR_CUSTOM_PROFILE_JS
+            .replace("__CUSTOM_PROFILE_DIR__", &profile_dir)
+            .replace("__MOD_ENTRYPOINT_FILE__", mod_entrypoint)
+    });
+
+    let custom_loader = if let Some(loader) = &mod_.loader {
+        let prefix = loader
+            .prefix
+            .as_ref()
+            .map(|p| {
+                p.replace("__CUSTOM_PROFILE_DIR__", &profile_dir)
+                    .replace("__MOD_ENTRYPOINT_FILE__", mod_entrypoint)
+            })
+            .unwrap_or_default();
+
+        let custom_profile = loader
+            .profile
+            .as_ref()
+            .map(|p| {
+                p.replace("__CUSTOM_PROFILE_DIR__", &profile_dir)
+                    .replace("__MOD_ENTRYPOINT_FILE__", mod_entrypoint)
+            })
+            .unwrap_or(profile_loader.unwrap_or_default());
+
+        let require = loader
+            .require
+            .as_ref()
+            .map(|r| {
+                r.replace("__CUSTOM_PROFILE_DIR__", &profile_dir)
+                    .replace("__MOD_ENTRYPOINT_FILE__", mod_entrypoint)
+            })
+            .unwrap_or(format!(r#"require("{}")"#, mod_entrypoint));
+
+        let suffix = loader
+            .suffix
+            .as_ref()
+            .map(|s| {
+                s.replace("__CUSTOM_PROFILE_DIR__", &profile_dir)
+                    .replace("__MOD_ENTRYPOINT_FILE__", mod_entrypoint)
+            })
+            .unwrap_or_default();
+
+        format!("{}\n{}\n{}\n{}", prefix, custom_profile, require, suffix)
+    } else {
+        format!(
+            "{}\nrequire(\"{}\")",
+            profile_loader.unwrap_or_default(),
+            mod_entrypoint
+        )
+    };
+
+    let mut asar = asar::AsarWriter::new();
+    asar.write_file("index.js", custom_loader, false).unwrap();
+    asar.write_file("package.json", include_bytes!("./asar/package.json"), false)
+        .unwrap();
+
+    asar.finalize(std::fs::File::create(&cached_asar_path).unwrap())
+        .unwrap();
+
+    cached_asar_path.to_str().unwrap().to_owned()
 }
